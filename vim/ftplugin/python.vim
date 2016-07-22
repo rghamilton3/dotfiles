b0892a26ea14ca5c1db542355d3ef9ed365b5a37
Robert Hamilton <rghamilton3@gmail.com>
2016-07-21 23:14:28 -0500

    Overhaul

---
+++ vim/ftplugin/python.vim
@@ -0,0 +1,10 @@
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set textwidth=79
+set expandtab
+set autoindent
+set fileformat=unix
+" Enable folding
+set foldmethod=indent
+set foldlevel=99
---
+++ zsh/aliases.zsh
@@ -0,0 +1,19 @@
+alias curl="curl -OL#"
+alias ping3="ping -c 3"
+alias cp='cp -iv'
+alias mv='mv -iv'
+alias rm='rm -iv'
+alias rmdir='rmdir -v'
+alias ln='ln -v'
+alias chmod="chmod -c"
+alias chown="chown -c"
+alias grep='grep --colour=auto'
+alias egrep='egrep --colour=auto'
+alias ls='ls --color=auto --human-readable --group-directories-first --classify'
+alias update='sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-upgrade && sudo apt-get autoremove'
+alias reboot='sudo reboot'
+alias shutdown='sudo shutdown -h now'
+
+alias ws="cd ~/workspace"
+alias cv="cd ~/workspace/cv"
+alias src="cd ~/src"
