1502a876946c8365e68208e15ec65c5f1f4fa399
Robert Hamilton <rghamilton3@gmail.com>
2016-07-31 20:50:27 -0500

    Zshenv file and install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim/ ~/.vim
+ln -s ~/.dotfiles/zsh/zshrc ~/.zshrc
+ln -s ~/.dotfiles/zsh/zshenv ~/.zshenv
+ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/tmux/ ~/.tmux
