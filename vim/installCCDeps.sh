2e9061d0b545f979b049d80ae930ae83fc5d18c2
Robert Hamilton <rghamilton3@gmail.com>
2017-06-17 22:04:19 -0500

    Add install scripts for YCM and Color Coded

---
+++ vim/installCCDeps.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+#
+# installCCDeps.sh
+# Copyright (C) 2017 rghamilton3 <rghamilton3@ArchTop>
+#
+# Distributed under terms of the MIT license.
+#
+
+installPkg() {
+    # Use Yaourt if installed
+    # No need to use sudo if so
+    if pacman -Qi yaourt > /dev/null
+    then
+        INSTALLER=yaourt
+    else
+        INSTALLER="sudo pacman"
+    fi
+
+    if ! pacman -Qi "$1" > /dev/null
+    then
+        echo "$1"
+    fi
+}
+
+installPkg "clang"
+installPkg "ncurses"
+installPkg "libz"
+installPkg "xz"
+installPkg "pthread_workqueue-git"
+installPkg "lua"
---
+++ vim/installYCM.sh
@@ -0,0 +1,94 @@
+#! /bin/sh
+#
+# installYCM.sh
+# Copyright (C) 2017 rghamilton3 <rghamilton3@ArchTop>
+#
+# > /dev/null redirects unneeded output from stdout
+# to nowhere
+#
+YCM_DIR="$HOME"/.vim/plugged/YouCompleteMe
+BUILD_DIR="$YCM_DIR"/build
+
+CLANG_VER=4.0.0
+CLANG_FILE=clang+llvm-"$CLANG_VER"-x86_64-linux-gnu-ubuntu-16.04
+CLANG_URL=http://releases.llvm.org/"$CLANG_VER"/"$CLANG_FILE".tar.xz
+
+installPkg() {
+    # Use Yaourt if installed
+    # No need to use sudo if so
+    if pacman -Qi yaourt > /dev/null
+    then
+        INSTALLER=yaourt
+    else
+        INSTALLER="sudo pacman"
+    fi
+
+    if ! pacman -Qi "$1" > /dev/null
+    then
+        echo "$1"
+    fi
+}
+
+mkBuildDir() {
+    if [ ! -d "$BUIlD_DIR" ]
+    then
+        mkdir -p "$BUILD_DIR"
+    fi
+}
+
+downloadClang() {
+    CLANG_DIR="$PWD"/"$CLANG_FILE"
+    echo "$CLANG_DIR"
+
+    if [ -d "$CLANG_FILE" ]
+    then
+        echo "$CLANG_FILE already exists"
+        return
+    fi
+
+    if  pacman -Qi wget > /dev/null 
+    then
+        wget "$CLANG_URL"
+    elif  pacman -Qi curl > /dev/null
+    then
+        curl "$CLANG_URL"
+    else
+        echo "Wget and Curl not found. What program should be used? (Just enter the cmd and flags without the url. No input exits)"
+        read DOWNLOADER
+        if [ -n "$DOWNLOADER" ]
+        then
+            "$DOWNLOADER" "$CLANG_URL"
+        else
+            echo "No program provided. Exiting"
+            exit
+        fi
+    fi
+
+    tar xf "$CLANG_FILE".tar.xz
+}
+
+cd "$YCM_DIR"
+mkBuildDir
+cd "$BUILD_DIR"
+downloadClang
+
+installPkg "cmake"
+installPkg "python"
+installPkg "python2"
+
+cmake -G "Unix Makefiles" -DPATH_TO_LLVM_ROOT="$CLANG_DIR" . "$YCM_DIR"/third_party/ycmd/cpp
+cmake --build . --target ycm_core --config Release
+
+# Install and build Node support
+installPkg "nodejs"
+installPkg "npm"
+cd "$YCM_DIR"/third_party/ycmd/third_party/tern_runtime
+npm install --production
+
+# Install and build Rust support
+installPkg "rustup"
+installPkg "rust-src"
+installPkg "cargo"
+rustup default stable
+cd "$YCM_DIR"/third_party/ycmd/third_party/racerd
+cargo build --release
