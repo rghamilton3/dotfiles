47fc979f56393bddc0c94f9192f7e8108b82be2e
Robert Hamilton <rghamilton3@gmail.com>
2017-06-25 22:53:59 -0500

    Fix antigen and aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,12 +1,12 @@
-alias curl="curl -OL#"
-alias ping3="ping -c 3"
+alias curl='curl -OL#'
+alias ping3='ping -c 3'
 alias cp='cp -iv'
 alias mv='mv -iv'
 alias rm='rm -iv'
 alias rmdir='rmdir -v'
 alias ln='ln -v'
-alias chmod="chmod -c"
-alias chown="chown -c"
+alias chmod='chmod -c'
+alias chown='chown -c'
 alias grep='grep --colour=auto'
 alias egrep='egrep --colour=auto'
 alias ls='ls --color=auto --human-readable --group-directories-first --classify'
@@ -14,10 +14,9 @@ alias update='sudo apt-get update && sudo apt-get upgrade && sudo apt-get dist-u
 alias reboot='sudo reboot'
 alias shutdown='sudo shutdown -h now'
 
-alias ws="cd ~/workspace"
-alias src="cd ~/source_builds"
-alias rstmp="sudo mount -o remount,size=8G,noatime /tmp"
-alias clntmp="su -c 'for i in /tmp/* ; do rm -r \"$i\" ; done'"
+alias ws='cd ~/workspace'
+alias src='cd ~/source_builds'
+alias rstmp='sudo mount -o remount,size=8G,noatime /tmp'
 
-alias algo="cd ~/workspace/algo"
-alias ac="g++ -pipe -O2 -std=c++14 -lm"
+alias algo='cd ~/workspace/algo'
+alias ac='g++ -pipe -O2 -std=c++14 -lm'
