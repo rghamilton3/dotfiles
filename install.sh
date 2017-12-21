9f0fd235738d5b133d19bb874c637fe6d47e13ab
Robert Hamilton <rghamilton3@gmail.com>
2017-12-21 14:30:26 -0600

    Refactor and add install software

--- install.sh
+++ install.sh
@@ -2,6 +2,23 @@
 
 INSTALL_DIR="$PWD"
 
+HOME_INSTALL=(xprofile vimrc vim zshrc zshenv tmux.conf tmux)
+
+echo "*** Installing required software from repos...\n"
+sudo apt install tmux zsh vim-gtk python{,3}-dev wget git
+
+echo "*** Installing Python requirements...\n"
+wget https://bootstrap.pypa.io/get-pip.py
+sudo -H python get-pip.py
+rm get-pip.py
+
+sudo -H pip install virtualenv virtualenvwrapper
+sudo rm -rf ~/get-pip.py ~/.cache/pip
+
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
+
+
 moveFileIfExists() {
     if [[ -h "$1" ]]  
     then
@@ -37,16 +54,13 @@ moveFileIfExists() {
     fi
 }
 
-moveFileIfExists "$HOME"/.xprofile
-ln -sv "$INSTALL_DIR"/xprofile "$HOME"/.xprofile
+echo "*** Installing config files and directories...\n"
+for item in ${HOME_INSTALL[@]}; do
+    moveFileIfExists "$HOME"/.${item}
+    ln -sv "$INSTALL_DIR"/${item} "$HOME"/.${item}
+done
 
 # VIM
-moveFileIfExists "$HOME"/.vimrc
-ln -sv "$INSTALL_DIR"/vim/vimrc "$HOME"/.vimrc
-
-moveFileIfExists "$HOME"/.vim
-ln -sv "$INSTALL_DIR"/vim/ "$HOME"/.vim
-
 if [[ -d "$INSTALL_DIR"/vim/autoload ]]
 then
     moveFileIfExists "$INSTALL_DIR"/vim/autoload/plug.vim
@@ -59,20 +73,6 @@ if [[ ! -d "$INSTALL_DIR"/vim/undo ]]
 then
     mkdir -p "$INSTALL_DIR"/vim/undo
 fi
-
-# ZSH
-moveFileIfExists "$HOME"/.zshrc
-ln -sv "$INSTALL_DIR"/zsh/zshrc "$HOME"/.zshrc
-
-moveFileIfExists "$HOME"/.zshenv
-ln -sv "$INSTALL_DIR"/zsh/zshenv "$HOME"/.zshenv
-
-# TMUX
-moveFileIfExists "$HOME"/.tmux.conf
-ln -sv "$INSTALL_DIR"/tmux/tmux.conf "$HOME"/.tmux.conf
-
-moveFileIfExists "$HOME"/.tmux
-ln -sv "$INSTALL_DIR"/tmux/ "$HOME"/.tmux
 
 # TERMITE
 if [[ ! -d "$HOME"/.config/termite ]]
