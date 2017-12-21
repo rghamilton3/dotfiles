e56d7ac08a227f7fa7c1129fa29dfd594c6db1b9
Robert Hamilton <rghamilton3@gmail.com>
2017-12-21 14:57:50 -0600

    Add select for installing software

--- install.sh
+++ install.sh
@@ -2,20 +2,20 @@
 
 INSTALL_DIR="$PWD"
 
-echo "*** Installing required software from repos..."
-sudo apt install tmux zsh vim-gtk python{,3}-dev wget git
+installPythonRequiremnts() {
+    echo "*** Installing pip..."
+    wget https://bootstrap.pypa.io/get-pip.py
+    sudo -H python get-pip.py
+    rm get-pip.py
 
-echo "*** Installing Python requirements..."
-wget https://bootstrap.pypa.io/get-pip.py
-sudo -H python get-pip.py
-rm get-pip.py
+    echo "*** Installing virtualenv..."
+    sudo -H pip install virtualenv
+    sudo rm -rf ~/get-pip.py ~/.cache/pip
 
-sudo -H pip install virtualenv
-sudo rm -rf ~/get-pip.py ~/.cache/pip
-
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
-
+    echo "*** Installing pyenv..."
+    git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+    git clone https://github.com/pyenv/pyenv-virtualenv.git ~/.pyenv/plugins/pyenv-virtualenv
+}
 
 moveFileIfExists() {
     if [[ -h "$1" ]]  
@@ -52,29 +52,54 @@ moveFileIfExists() {
     fi
 }
 
+echo "Install required software from repos?";
+select ans in "Yes" "No" "Quit"; do
+    case $ans in
+        Yes )
+            sudo apt install tmux zsh vim-gtk python{,3}-dev wget git;
+            break;;
+        No )
+            break;;
+        Quit )
+            echo "Quitting...";
+            exit;;
+    esac
+done
+
+echo "Install Python requirements?"
+select ans in "Yes" "No" "Quit"; do
+    case $ans in
+        Yes )
+            installPythonRequiremnts
+            break;;
+        No )
+            break;;
+        Quit )
+            echo "Quitting...";
+            exit;;
+    esac
+done
+
 echo "*** Installing config files and directories..."
+
+# MISC
 moveFileIfExists "$HOME/.xprofile"
 ln -sv "$INSTALL_DIR/xprofile" "$HOME/.xprofile"
 
-moveFileIfExists "$HOME/.vimrc"
-ln -sv "$INSTALL_DIR/vim/vimrc" "$HOME/.vimrc"
-
-moveFileIfExists "$HOME/.vim"
-ln -sv "$INSTALL_DIR/vim" "$HOME/.vim"
-
+# ZSH
 moveFileIfExists "$HOME/.zshrc"
 ln -sv "$INSTALL_DIR/zsh/zshrc" "$HOME/.zshrc"
 
 moveFileIfExists "$HOME/.zshenv"
 ln -sv "$INSTALL_DIR/zsh/zshenv" "$HOME/.zshenv"
 
-moveFileIfExists "$HOME/.tmux.conf"
-ln -sv "$INSTALL_DIR/tmux/tmux.conf" "$HOME/.tmux.conf"
-
-moveFileIfExists "$HOME/.tmux"
-ln -sv "$INSTALL_DIR/tmux" "$HOME/.tmux"
-
 # VIM
+moveFileIfExists "$HOME/.vimrc"
+ln -sv "$INSTALL_DIR/vim/vimrc" "$HOME/.vimrc"
+
+moveFileIfExists "$HOME/.vim"
+ln -sv "$INSTALL_DIR/vim" "$HOME/.vim"
+
 if [[ -d "$INSTALL_DIR"/vim/autoload ]]
 then
     moveFileIfExists "$INSTALL_DIR"/vim/autoload/plug.vim
@@ -87,6 +112,13 @@ if [[ ! -d "$INSTALL_DIR"/vim/undo ]]
 then
     mkdir -p "$INSTALL_DIR"/vim/undo
 fi
+
+# TMUX
+moveFileIfExists "$HOME/.tmux.conf"
+ln -sv "$INSTALL_DIR/tmux/tmux.conf" "$HOME/.tmux.conf"
+
+moveFileIfExists "$HOME/.tmux"
+ln -sv "$INSTALL_DIR/tmux" "$HOME/.tmux"
 
 # TERMITE
 if [[ ! -d "$HOME"/.config/termite ]]
