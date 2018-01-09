4cc06f0517e9d1da43505d2a62bd761834a24678
Robert Hamilton <rghamilton3@gmail.com>
2018-01-08 18:05:54 -0600

    Update ycm install

--- vim/installYCM.sh
+++ vim/installYCM.sh
@@ -13,22 +13,6 @@ CLANG_VER=5.0.1
 CLANG_FILE=clang+llvm-"$CLANG_VER"-x86_64-linux-gnu-ubuntu-14.04
 CLANG_URL=http://releases.llvm.org/"$CLANG_VER"/"$CLANG_FILE".tar.xz
 
-installPkg() {
-    # Use pacaur if installed
-    # No need to use sudo if so
-    if pacman -Qi yaourt > /dev/null
-    then
-        INSTALLER=pacaur
-    else
-        INSTALLER="sudo pacman"
-    fi
-
-    if ! pacman -Qi "$1" > /dev/null
-    then
-        "$INSTALLER" -Sy "$1"
-    fi
-}
-
 mkBuildDir() {
     if [ ! -d "$BUIlD_DIR" ]
     then
@@ -71,10 +55,6 @@ cd "$YCM_DIR"
 mkBuildDir
 cd "$BUILD_DIR"
 #downloadClang
-
-installPkg "cmake"
-installPkg "python"
-installPkg "python2"
 
 cmake -G "Unix Makefiles" -DUSE_SYSTEM_LIBCLANG=ON . "$YCM_DIR"/third_party/ycmd/cpp
 cmake --build . --target ycm_core --config Release
