80c905af71e5c70de212a168ce187d9ab6d12794
Robert Hamilton <rghamilton3@gmail.com>
2024-05-13 11:57:26 -0500

    Update .config/fish/config.fish

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,6 +1,10 @@
 set -gx PATH $PATH "/home/rghamilton3/.local/bin"
 set -gx PATH $PATH "/home/rghamilton3/go/bin"
 set -gx PATH $PATH "/home/rghamilton3/.local/share/JetBrains/Toolbox/scripts"
+set -gx EDITOR lvim
+alias nvim="lvim"
+alias vim="lvim"
+alias vi="lvim"
 
 if status is-interactive
     mise activate fish | source
@@ -13,7 +17,7 @@ set -gx GPG_TTY (tty)
 zoxide init --cmd cd fish | source
 
 abbr -a -- pn pnpm
-abbr -a -- n nvim
+abbr -a -- n lvim
 abbr -a -- gp 'git push'
 abbr -a -- gpa 'git push -u origin --all'
 abbr -a -- gs 'git status'
