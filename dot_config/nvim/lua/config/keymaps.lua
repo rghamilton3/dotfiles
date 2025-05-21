c0e0e2dc2b74045fcb20417211f23df0858d0da1
Robert Hamilton <robert@rghsoftware.com>
2025-05-20 23:24:56 -0500

    Add .config/fish/functions/ong.fish Update .config/nvim/lua/config/keymaps.lua

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -34,6 +34,7 @@ map("x", "p", [["_dP]])
 map("n", "<Down>", "5j", { desc = "Move down 5 lines" })
 map("n", "<Up>", "5k", { desc = "Move up 5 lines" })
 
+-- For Markdown
 -- Toggle checkbox values
 local function toggle_checkbox()
   -- Get the current line
@@ -58,17 +59,25 @@ local function toggle_checkbox()
   end
   vim.api.nvim_set_current_line(new_line)
 end
-
-map("n", "<leader>ch", toggle_checkbox, { desc = "Toggle checkbox", noremap = true, silent = true })
+map("n", "<leader>ch", toggle_checkbox, { desc = "Toggle checkbox" })
 
 map("n", "<leader>fo", function()
   Snacks.picker.files({
     cwd = vim.fn.expand("~") .. "/Vaults/Second Brain",
   })
 end, { desc = "Find note", noremap = true, silent = true })
-
 map("n", "<leader>fO", function()
   Snacks.picker.grep({
     cwd = vim.fn.expand("~") .. "/Vaults/Second Brain",
   })
 end, { desc = "Find note text", noremap = true, silent = true })
+
+-- Note taking
+map("n", "<leader>oo", ":cd /home/rghamilton3/Vaults/Second Brain<cr>", { desc = "Navigate to notes" })
+map(
+  "n",
+  "<leader>ok",
+  ":!mv '%:p' /home/rghamilton3/Vaults/Second Brain/zk<cr>:bd<cr>",
+  { desc = "Move to zettlekasten" }
+)
+map("n", "<leader>odd", ":!rm '%:p'<cr>:bd<cr>", { desc = "Delete note" })
